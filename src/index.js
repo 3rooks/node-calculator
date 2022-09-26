c986714af9d7e50b42aaeaa4f5244472ecd5e551
3rooks <Brookslynx@gmail.com>
2022-09-26 20:44:58 -0300

    🚧 Refactoring & dealing with errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+import { InvalidInputError } from '#errors/invalidInputError.js';
+import { getOperator } from '#lib/getOperator.js';
 import { promptQuestion } from '#lib/promptQuestion.js';
 
 (async () => {
-    const userAnswer = await promptQuestion('intruduce tu operacion: ');
-    console.log(userAnswer);
+    try {
+        const userAnswer = await promptQuestion('intruduce tu operacion: ');
+
+        const standarizeInput = userAnswer.trim();
+
+        if (standarizeInput === '') throw new InvalidInputError();
+
+        const operator = getOperator(standarizeInput);
+
+        if (!operator) throw new InvalidInputError();
+    } catch (error) {
+        if (error instanceof InvalidInputError) console.log(error.message);
+        else console.log(`Error: ${error.message} - Stack: ${error.stack}`);
+    }
 })();
